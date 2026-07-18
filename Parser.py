9de0b763bde9f9aa36a278a081d4e6a2a56a52a6
jamalgattu <jamalgattu@gmail.com>
2026-07-18 17:42:41 +0530

    Use environment variable for URL
    Replace hardcoded URL with environment variable.

--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://sarkariresult.com.cm/feed/"
+url = os.environ.get("URL")
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 CHANNEL_ID = os.environ.get("TELEGRAM_CHANNEL_ID")
